cb27c7c892e1b731af7d3f4c1ba5454a69652aee
xuguanjie <xuguanjie@halove.com>
2020-09-28 14:56:20 +0800

    代码上传

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 export default {
-  url: process.env.NODE_ENV === 'development' ? 'http://localhost:7001/' : 'http://localhost:7001/',
+  url: process.env.NODE_ENV === 'development' ? 'http://localhost:7001/' : 'http://pick.yystart.com/',
   webUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:8001/#/Exchange/' : 'http://pick.yystart.com/mobile/#/Exchange/',
   // url: process.env.NODE_ENV === 'development' ? 'http://106.14.182.16:7001/' : 'http://localhost:8000/'
 };
